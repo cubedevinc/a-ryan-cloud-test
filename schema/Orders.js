39be36fe453fd1cc1dedcd4372f9dac66eb723fc
ryan <ryan@cube.dev>
2021-08-04 22:33:15 +0000

    test

--- schema/Orders.js
+++ schema/Orders.js
@@ -12,6 +12,7 @@ cube(`Orders`, {
   `,
 
   preAggregations: {
+    //test
     // Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
   },
