7bc71d52092990f0d7a02afb3310948737d8d07f
ryan <ryan@cube.dev>
2021-08-04 22:29:35 +0000

    test

--- cube.js
+++ cube.js
@@ -1,36 +1,3 @@
-//const SnowflakeDriver = require('@cubejs-backend/snowflake-driver');
-
+// Cube.js configuration options: https://cube.dev/docs/config
 module.exports = {
-/*
-  queryTransformer: (query, { securityContext }) => {
-  const { team } = securityContext;
-  if (team.id) {
-    query.filters.push({
-      member: 'Events.teamId',
-      operator: 'equals',
-      values: [team.id],
-    });
-  }
-  return query;
-},
-*/
-/*
-  scheduledRefreshContexts: async () => [
-    {
-      securityContext: {
-        tenantId: 'TPCH_SF1',
-      },
-    },
-  ],
-  contextToAppId: ({ securityContext }) =>
-  `CUBEJS_APP_${securityContext.tenantId}`,
-  preAggregationsSchema: ({ securityContext }) =>
-  `pre_aggregations_${securityContext.tenantId}`
-*/
-  /*
-  driverFactory: () =>
-    new SnowflakeDriver({
-      readOnly: true,
-    }),
-    */
 };
